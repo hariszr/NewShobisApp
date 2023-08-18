17c6cb45f70ff82573be64a2b9db326491680dcc
Muhammad Haris Amin <muhammadharis.zero150@gmail.com>
2023-08-18 11:13:06 +0700

    Revision: 4. Berhasil menampilkan seluruh total capital, income, and profit dengan menggabungkan spinner pada waktu bulan ini, semua waktu, dan bulan lalu dari firebase path "summary". to be continued...

--- app/src/main/java/com/example/loginandsignupfirebase/SummaryActivity.kt
+++ app/src/main/java/com/example/loginandsignupfirebase/SummaryActivity.kt
@@ -5,6 +5,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.GridLayoutManager
@@ -15,6 +18,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import java.text.DecimalFormat
 import java.text.NumberFormat
+import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -49,7 +53,13 @@ class SummaryActivity : AppCompatActivity() {
         }
 
         showSummary()
+        spinnerSort()
 
+//        recyclerViewShow()
+
+    }
+
+    private fun recyclerViewShow() {
         val gridLayoutManager =GridLayoutManager(this, 1)
         binding.recyclerView.layoutManager = gridLayoutManager
 
@@ -60,7 +70,6 @@ class SummaryActivity : AppCompatActivity() {
         dialog.show()
 
         dataList = ArrayList()
-
 
         adapter = ListAdapterSummary(this, dataList)
         binding.recyclerView.adapter = adapter
@@ -85,7 +94,137 @@ class SummaryActivity : AppCompatActivity() {
                 dialog.dismiss()
             }
         })
+    }
 
+    private fun recyclerViewShowThisMonth() {
+        val gridLayoutManager =GridLayoutManager(this, 1)
+        binding.recyclerView.layoutManager = gridLayoutManager
+
+        val builder = AlertDialog.Builder(this)
+        builder.setCancelable(false)
+        builder.setView(R.layout.layout_progress)
+        val dialog = builder.create()
+        dialog.show()
+
+        dataList = ArrayList()
+
+        adapter = ListAdapterSummary(this, dataList)
+        binding.recyclerView.adapter = adapter
+        val databaseReferenceChild = databaseReference.child(firebaseAuth.uid.toString()).child("summary")
+        dialog.show()
+
+        eventListener = databaseReferenceChild.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                dataList.clear()
+                val currentMonth = Calendar.getInstance().get(Calendar.MONTH)
+
+                for (itemSnapshot in snapshot.children) {
+                    val childDate = SimpleDateFormat("MMM dd, yyyy HH:mm:ss", Locale.ENGLISH).parse(itemSnapshot.key.toString())
+                    val childCalendar = Calendar.getInstance()
+                    if (childDate != null) {
+                        childCalendar.time = childDate
+                    }
+
+                    val childMonth = childCalendar.get(Calendar.MONTH)
+
+                    if (childMonth == currentMonth) {
+                        val dataClassSummary = itemSnapshot.getValue(DataClassSummary::class.java)
+                        if (dataClassSummary != null) {
+                            dataList.add(dataClassSummary)
+                        }
+                    }
+                }
+
+                adapter.notifyDataSetChanged()
+                dialog.dismiss()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                dialog.dismiss()
+            }
+        })
+    }
+
+    private fun recyclerViewShowLastMonth() {
+        val gridLayoutManager =GridLayoutManager(this, 1)
+        binding.recyclerView.layoutManager = gridLayoutManager
+
+        val builder = AlertDialog.Builder(this)
+        builder.setCancelable(false)
+        builder.setView(R.layout.layout_progress)
+        val dialog = builder.create()
+        dialog.show()
+
+        dataList = ArrayList()
+
+        adapter = ListAdapterSummary(this, dataList)
+        binding.recyclerView.adapter = adapter
+        val databaseReferenceChild = databaseReference.child(firebaseAuth.uid.toString()).child("summary")
+        dialog.show()
+
+        eventListener = databaseReferenceChild.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                dataList.clear()
+                val currentMonth = Calendar.getInstance().get(Calendar.MONTH)
+                val lastMonth = (currentMonth - 1 + 12) % 12 // Menghitung bulan lalu dengan memperhitungkan overflow
+
+                for (itemSnapshot in snapshot.children) {
+                    val childDate = SimpleDateFormat("MMM dd, yyyy HH:mm:ss", Locale.ENGLISH).parse(itemSnapshot.key.toString())
+                    val childCalendar = Calendar.getInstance()
+                    if (childDate != null) {
+                        childCalendar.time = childDate
+                    }
+
+                    val childMonth = childCalendar.get(Calendar.MONTH)
+
+                    if (childMonth == lastMonth) {
+                        val dataClassSummary = itemSnapshot.getValue(DataClassSummary::class.java)
+                        if (dataClassSummary != null) {
+                            dataList.add(dataClassSummary)
+                        }
+                    }
+                }
+
+                adapter.notifyDataSetChanged()
+                dialog.dismiss()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                dialog.dismiss()
+            }
+        })
+    }
+
+    private fun spinnerSort() {
+        val time = arrayOf("Bulan Ini", "Bulan Lalu", "Semua Waktu")
+        val adapterTime = ArrayAdapter(this, com.bumptech.glide.R.layout.support_simple_spinner_dropdown_item, time)
+        binding.spinnerSort.adapter = adapterTime
+        binding.spinnerSort.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selectedOption = time[position]
+                if (selectedOption == "Bulan Ini") {
+                    recyclerViewShowThisMonth()
+                }
+
+                if (selectedOption == "Bulan Lalu") {
+                    recyclerViewShowLastMonth()
+                }
+
+                if (selectedOption == "Semua Waktu") {
+                    recyclerViewShow()
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                recyclerViewShow()
+            }
+
+        }
     }
 
     private fun showSummary() {
